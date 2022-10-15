fdb5703eaa181e62c2c8e83c8e6c10d3d87d284d
Luis Tapia <devlou@protonmail.com>
2022-10-14 20:15:29 -0500

    Add ES6: strings

---
+++ ecmascript/src/es6/02-nothing-lol.js
@@ -0,0 +1,4 @@
+// There's nothing here lol
+
+// Remember to drink water
+// Otherwise you'll be dead
---
+++ ecmascript/src/es6/03-strings.js
@@ -0,0 +1,19 @@
+// Template literals
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multiline strings
+
+let lorem = "This is a string \n" + "This is another line";
+console.log(lorem);
+
+let lorem2 = `This is an epic phrase
+and this is its continuation.
+`;
+console.log(lorem2);
