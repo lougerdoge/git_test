ba03d5cb848c612afb9481f5aac50c3259177ceb
Luis Tapia <devlou@protonmail.com>
2022-10-15 20:34:28 -0500

    Add ES6: destructuring

---
+++ ecmascript/src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// Arrays destructuring
+
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// Object destructuring
+
+let user = { username: "Oscar", age: 34 };
+let { username, age } = user;
+console.log(username, user.age);
