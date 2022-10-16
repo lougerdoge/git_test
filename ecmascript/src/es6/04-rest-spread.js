b99ff25df9f3422469c10ee1a0757e233bad199a
Luis Tapia <devlou@protonmail.com>
2022-10-15 20:42:59 -0500

    Add ES6: spread operator

--- ecmascript/src/es6/04-rest-spread.js
+++ ecmascript/src/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, fruits[1]);
 let user = { username: "Oscar", age: 34 };
 let { username, age } = user;
 console.log(username, user.age);
+
+// Spread operator
+
+let person = { name: "Oscar", age: 28 };
+let country = "MX";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// Rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
