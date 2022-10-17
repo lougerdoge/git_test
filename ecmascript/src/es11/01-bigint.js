12370a9e10bd17ac5e3110762e68a020a6a590c2
Luis Tapia <devlou@protonmail.com>
2022-10-16 23:21:01 -0500

    Add ES11: BigInt and Nullish

---
+++ ecmascript/src/es11/01-bigint.js
@@ -0,0 +1,3 @@
+const aBigNumber = 7897598375982735832792492384n;
+const anotherBigNumber = BigInt(7897598375982735832792492384);
+console.log(aBigNumber, anotherBigNumber);
---
+++ ecmascript/src/es11/02-nullish.js
@@ -0,0 +1,6 @@
+const numberOne = 1;
+const numberTwo = null;
+const validateOne = numberOne ?? 5;
+const validateTwo = numberTwo ?? 5;
+console.log(validateOne);
+console.log(validateTwo);
